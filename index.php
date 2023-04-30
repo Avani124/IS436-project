5a2fe0f1329efbeb7b22a93536ba439bc509cd2d
Avani124 <avanir1@umbc.edu>
2023-04-30 13:08:54 -0400

    php files

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-UA-Compatible" content="IE=edge">
+    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Pizza System</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Pizza Delivery System</h1>
+      <a href="./signUp.html"><h2>Sign Up</h2></a>
+      <a href="./login.html"><h2>Login Up</h2></a>
+    </div>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+      <title>Login Form</title>
+      <link rel="stylesheet" href="style.css">
+    </head>
+      <body>
+        <div class ="login-form">
+          
+          <h1> Login Now </h1>
+          <form>
+            <input type="email" class="input-box" placeholder="Your Email">
+            <input type="email" class="input-box" placeholder="Your Password">
+
+            <p>I agree to the terms of services</p>
+            <p class="submit"><input type = "submit" name = "commit" value="Login"></p>
+            
+          </form>
+        </div>
+      </body>
+  </html>
